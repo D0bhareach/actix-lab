07851e90d41a2961c84aa3476600f79490bc1631
d0bhareach <viacheslav.modestov@gmail.com>
2023-01-02 17:37:37 +0100

    Add TLS support for lab

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ I would like to try Actix Framework. The server I would like to build must have:
 * Sessions.
 * Error Logging System.
 * Cookies
+
+## TLS
+To create tls certificate and key files install [`mkcert.`]: https://github.com/FiloSottile/mkcert
+Run `mkcert localhost 127.0.0.1` it will create sertificatate and key suitable for testing
+and developmen.  
+Current certificates are valid until 2025-04-02.  
+When site is accessed browser will throw warning about certificate. Because certificate is not
+issued by known autohority.
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-mod page;
 mod error;
+mod page;
 use actix_files::Files;
-use actix_web::{middleware, post, web, App, HttpResponse, HttpServer, Responder,
-    http::StatusCode
+use actix_web::{
+    http::StatusCode, middleware, post, web, App, HttpResponse, HttpServer, Responder,
 };
-use page::{index, error as err, login};
-use error::handler::not_found_handler;
-// TODO:
-// create pages modules: index, user, login, error.
-// in modules create services and attach middleware for wrapping tera render to responce
-// in error mod create handlers for 404 and 500. Add error handlers to root in app.
+use error::handler::{internal_error_handler, not_found_handler};
+use page::{error as err, index, login};
+use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{certs, pkcs8_private_keys};
+use std::{fs::File, io::BufReader};
+
+// TODO: Add Sessions
+// TODO: Add Cookie Middleware. For Session Persistence.
+// TODO: add redis for holding session data.
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
@@ -20,11 +23,45 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+fn load_rustls_config() -> rustls::ServerConfig {
+    // init server config builder with safe defaults
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth();
+
+    // load TLS key/cert files
+    let cert_file = &mut BufReader::new(File::open("certificate/cert.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("certificate/key.pem").unwrap());
+
+    // convert files to key/cert objects
+    let cert_chain = certs(cert_file)
+        .unwrap()
+        .into_iter()
+        .map(Certificate)
+        .collect();
+    let mut keys: Vec<PrivateKey> = pkcs8_private_keys(key_file)
+        .unwrap()
+        .into_iter()
+        .map(PrivateKey)
+        .collect();
+
+    // exit if no keys could be parsed
+    if keys.is_empty() {
+        eprintln!("Could not locate PKCS 8 private keys.");
+        std::process::exit(1);
+    }
+
+    config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
+}
+
+// TODO: how to test if browser is caching, how to test no-cache??
+// TODO: First test for span / domain shall be headers and cookies tests.
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info, actix_lab=trace");
     // env_logger::init_from_env(Env::default().default_filter_or("info"));
     env_logger::init();
+    let tls_config = load_rustls_config();
 
     HttpServer::new(|| {
         // there is one more instance of tera with exact the same settings in handlers for errors
@@ -32,17 +69,20 @@ async fn main() -> std::io::Result<()> {
             tera::Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*.html")).unwrap();
         App::new()
             .app_data(web::Data::new(tera))
-            .wrap(middleware::ErrorHandlers::new()
-                .handler(StatusCode::NOT_FOUND, not_found_handler),)
+            .wrap(
+                middleware::ErrorHandlers::new()
+                    .handler(StatusCode::NOT_FOUND, not_found_handler)
+                    .handler(StatusCode::INTERNAL_SERVER_ERROR, internal_error_handler),
+            )
             .wrap(middleware::Logger::default())
             .service(Files::new("/public", "static"))
             .service(index::index_scope())
-            .service(login::login_scope())
+            .service(login::login_scope().wrap(login::default_headers()))
             .service(err::error_scope())
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8081))?
+    .bind_rustls("127.0.0.1:8443", tls_config)?
     .run()
     .await
 }
