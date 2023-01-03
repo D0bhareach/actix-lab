b1ffd2828a4e65fd6a73552b7ae29c5ca8a2a07b
d0bhareach <viacheslav.modestov@gmail.com>
2023-01-03 11:42:35 +0100

    Start to do loging:

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,23 +1,27 @@
 pub mod handler;
-use actix_web::{ResponseError,};
+use actix_web::{HttpResponse, ResponseError};
 use thiserror::Error as ThisError;
 
 #[derive(Debug, ThisError)]
 pub enum ActixLabError {
     #[error("actix_lab template error")]
-    TemplateError(#[from]tera::Error),
+    TemplateError(#[from] tera::Error),
 }
-use actix_web::http::StatusCode; 
+use actix_web::http::StatusCode;
 
 impl ResponseError for ActixLabError {
     fn status_code(&self) -> StatusCode {
         match self {
-              /*
+            /*
               ActixLabErrr::ValidationError(_) => StatusCode::BAD_REQUEST,
               ActixLabError::DatabaseError(_)
             | ActixLabError::StoreTokenError(_)
             */
             ActixLabError::TemplateError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
+    }
+
+    fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
+        HttpResponse::new(self.status_code())
     }
 }
--- src/page/login.rs
+++ src/page/login.rs
@@ -1,24 +1,90 @@
-use crate::error::ActixLabError;
-use actix_web::{
-    web, Result, Responder, Error, Scope,
-};
-use actix_web_lab::respond::Html;
+use std::collections::HashMap;
 
-async fn login(tmpl: web::Data<tera::Tera>) -> Result<impl Responder, Error> {
-    let ctx = tera::Context::new();
+use crate::error::ActixLabError;
+use actix_web::{http::StatusCode, middleware, web, Error, HttpResponse, Responder, Result, Scope};
+use actix_web_lab::respond::Html;
+use log;
+use serde::Deserialize;
+
+// login_post to get user from somewhere and set session
+// in case of an error load page from template and add context.
+pub fn default_headers() -> middleware::DefaultHeaders {
+    let h = middleware::DefaultHeaders::new();
+    h.add(("CacheControl", r#"no-cache="SetCookie, SetCookie2""#))
+}
+
+fn render_login_page(
+    tmpl: web::Data<tera::Tera>,
+    context: Option<tera::Context>,
+) -> Result<String, Error> {
+    let ctx = if let Some(ctx) = context {
+        ctx
+    } else {
+        tera::Context::new()
+    };
     let res = tmpl.render("login.html", &ctx).map_err(|e| {
-        log::error!("{}", e);
+        log::error!("Error rendering index.html. {}", e);
         ActixLabError::TemplateError(e)
     })?;
+    Ok(res)
+}
 
-    Ok(Html(res))
+async fn login(tmpl: web::Data<tera::Tera>) -> Result<impl Responder, Error> {
+    Ok(Html(render_login_page(tmpl, None)?))
 }
 
 async fn logout(_tmpl: web::Data<tera::Tera>) -> Result<impl Responder, Error> {
     Ok("Logged out.")
 }
+
+#[derive(Deserialize)]
+struct LoginForm {
+    username: String,
+    password: String,
+}
+
+fn check_user(form: web::Form<LoginForm>) -> bool {
+    let mut map = HashMap::with_capacity(2);
+    map.insert("admin", "pass");
+    map.insert("d0bhareach", "pass");
+
+    let name = form.username.clone();
+    let pass = form.password.clone();
+    if let Some(mpass) = map.get(name.as_str()) {
+        return *mpass == pass;
+    }
+    false
+}
+
+async fn login_post(
+    // when success must create Session.
+    form: web::Form<LoginForm>,
+    t: web::Data<tera::Tera>,
+) -> Result<impl Responder, Error> {
+    // validate user input if success go to index page.
+    if check_user(form) {
+        return Ok(HttpResponse::Found()
+            .append_header(("Location", "/"))
+            .finish());
+    }
+    let mut ctx = tera::Context::new();
+    ctx.insert("message", &"Error message. Something went wrong.");
+    let template = render_login_page(t, Some(ctx));
+
+    Ok(actix_web::HttpResponseBuilder::new(StatusCode::OK)
+        .content_type("html")
+        .body(template?))
+}
+
 pub fn login_scope() -> Scope {
     web::scope("/session")
-    .service(web::resource("/login").route(web::get().to(login)))
-    .service(web::resource("/logout").route(web::get().to(logout)))
+        // this method is returning Scope but with other generic arguments see above.
+        // .wrap(middleware::DefaultHeaders::new())
+        .service(
+            web::resource("/login")
+                .route(web::get().to(login))
+                .route(web::post().to(login_post)),
+        )
+        // .service(web::resource("/login").route(web::post().to(login_post)))
+        .service(web::resource("/logout").route(web::get().to(logout)))
 }
